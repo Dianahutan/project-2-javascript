f123f641673c4b62984792b5bc75cd9abe2e38f7
Diana Hutan <dianahutange@gmail.com>
2023-05-09 01:22:07 +0200

    added a footer, and css for; also added 2 placedholders and css for the API content

--- main.js
+++ main.js
@@ -22,37 +22,41 @@ const createElemment = (itemData) => {
   containerElement.classList.add("main")
 
   const containerElementCard = document.createElement("div")
-  containerElement.classList.add("element_card")
+  containerElementCard.classList.add("element_card")
 
   const imageEl = document.createElement("img")
+  imageEl.classList.add("photo_element")
+
   const titleEl = document.createElement("h1")
   titleEl.classList.add("product_title")
 
-  const overviewEl = document.createElement("p")
-  overviewEl.classList.add("overview_element")
+  /* const overviewEl = document.createElement("p")
+  overviewEl.classList.add("overview_element") */
 
-  const priceEl = document.createElement("p")
-  const buyEl = document.createElement("buttton")
+  const priceEl = document.createElement("div")
+
+  const containerButtonElement= document.createElement("div")
+  const buyEl = document.createElement("button")
+  containerButtonElement.classList.add("buy_button")
 
   imageEl.src = itemData.api_featured_image
   imageEl.style.width = "150px"
   titleEl.innerText = itemData.name
-  overviewEl.innerText = itemData.description.split("\n").join("")
+  /* overviewEl.innerText = itemData.description.split("\n").join("") */
   buyEl.innerText = "Buy now!"
   priceEl.innerText = itemData.price + " €"
 
   containerElement.appendChild(containerElementCard)
   containerElementCard.appendChild(imageEl)
   containerElementCard.appendChild(titleEl)
-  containerElementCard.appendChild(overviewEl)
+  /* containerElementCard.appendChild(overviewEl) */
   containerElementCard.appendChild(priceEl)
-  containerElementCard.appendChild(buyEl)
+  containerElement.appendChild(containerButtonElement)
+  containerButtonElement.appendChild(buyEl)
 
   return containerElement
 }
 
+
 createList()
 
-/*data.filter(function (value) {
-  return value="bronzer"
-  })  */
